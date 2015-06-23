f581965531b4736226c459f176a917447ed13da3
Ivan Tsekhmistro <itsekhmistro@adyax.com>
2015-06-23 15:00:18 +0300

    fixed project directory structure

--- files/drupal7/scripts/reinstall.sh
+++
@@ -1,5 +0,0 @@
-if [ "${1}"=="--windows" ]; then
- time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "is_windows=true"
-else
- time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local
-fi
